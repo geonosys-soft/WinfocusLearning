2b5e7e208d7dc98b402114c422c430ffa5532a4c
ABDUL SAMAD EK KATTUPPARA <samadekktp@gmail.com>
2025-07-18 21:52:50 +0530

    9.52

---
+++ WinfocusLearningApp/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WinfocusLearningApp.Models;
+
+namespace WinfocusLearningApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login() { 
+            LoginModel inf=new LoginModel();
+            inf.Username = "admin";
+            inf.Password = "1234";
+           if(inf.Username=="admin" && inf.Password=="123")
+            {
+                FormsAuthentication formsAuthentication = new FormsAuthentication();    
+
+            }
+        return View();
+        }
+    }
+}
